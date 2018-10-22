12473624977666ed0588751efdbf587e152132f7
Marco Barão <marco.barao@outlook.com>
2018-10-22 10:43:25 -0300

    CORS

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(
