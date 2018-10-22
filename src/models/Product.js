7cc46b687c0d3424feb9326d26f540986cbca55b
Marco Barão <marco.barao@outlook.com>
2018-10-22 10:37:00 -0300

    Paginação da lista

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const paginate = require("mongoose-paginate");
 
 const ProductSchema = new mongoose.Schema({
   title: {
@@ -18,5 +19,7 @@ const ProductSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+
+ProductSchema.plugin(paginate);
 
 mongoose.model("Product", ProductSchema);
